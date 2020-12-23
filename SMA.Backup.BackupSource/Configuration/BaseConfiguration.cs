41a4bcdb6206df9db3f6f1bda40cedff58a69c98
saeedmaghdam <me.register@outlook.com>
2020-12-23 21:48:43 +0330

    Added backup configuration

---
+++ SMA.Backup.BackupSource/Configuration/BaseConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using SMA.Backup.BackupSource.Framework;
+
+namespace SMA.Backup.BackupSource.Configuration
+{
+    public abstract class BaseConfiguration : IBackupSourceConfiguration
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
