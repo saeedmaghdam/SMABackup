c4fa09d8a03e1356e94639368191d97e723f4a22
saeedmaghdam <me.register@outlook.com>
2020-12-23 21:48:43 +0330

    Added backup output model

---
+++ SMA.Backup.BackupSource/Model/OutputModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SMA.Backup.BackupSource.Model
+{
+    public class OutputModel
+    {
+        public DateTime FileCreationDate
+        {
+            get;
+            set;
+        }
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+        public string FileExtension
+        {
+            get;
+            set;
+        }
+
+        public string Path
+        {
+            get;
+            set;
+        }
+
+        public long FileSize
+        {
+            get;
+            set;
+        }
+
+        public string FileHash
+        {
+            get;
+            set;
+        }
+    }
+}
