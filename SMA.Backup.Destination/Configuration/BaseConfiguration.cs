b9c67196e19686ec4c8e1991305311da23e0bd94
saeedmaghdam <me.register@outlook.com>
2020-12-23 21:48:44 +0330

    Added base configuration

---
+++ SMA.Backup.Destination/Configuration/BaseConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using SMA.Backup.Destination.Framework;
+
+namespace SMA.Backup.Destination.Configuration
+{
+    public abstract class BaseConfiguration : IDestinationConfiguration
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+    }
+}
