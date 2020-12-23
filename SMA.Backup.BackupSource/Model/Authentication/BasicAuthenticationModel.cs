f7673a2236de3805a897bf485785cd75bb709a26
saeedmaghdam <me.register@outlook.com>
2020-12-23 21:48:43 +0330

    Added basic authentication

---
+++ SMA.Backup.BackupSource/Model/Authentication/BasicAuthenticationModel.cs
@@ -0,0 +1,19 @@
+﻿using SMA.Backup.BackupSource.Framework;
+
+namespace SMA.Backup.BackupSource.Model.Authentication
+{
+    public class BasicAuthenticationModel : IAuthenticationModel
+    {
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+    }
+}
