010d35824ee36d836c68d78f4287845c83c24394
saeedmaghdam <me.register@outlook.com>
2020-12-23 21:48:43 +0330

    Added backup source interface

---
+++ SMA.Backup.BackupSource/Framework/IBackupSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SMA.Backup.BackupSource.Model;
+
+namespace SMA.Backup.BackupSource.Framework
+{
+    public interface IBackupSource
+    {
+        Task<OutputModel> Backup();
+    }
+}
