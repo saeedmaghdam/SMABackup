017534e4103c850b9443efce986081805019c2bf
saeedmaghdam <me.register@outlook.com>
2020-12-23 21:48:44 +0330

    Added google drive destination source

---
+++ SMA.Backup.Destination/Configuration/GoogleDriveConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace SMA.Backup.Destination.Configuration
+{
+    public class GoogleDriveConfiguration : BaseConfiguration
+    {
+        public string ClientId
+        {
+            get;
+            set;
+        }
+
+        public string ClientSecret
+        {
+            get;
+            set;
+        }
+
+        public string ApplicationName
+        {
+            get;
+            set;
+        }
+    }
+}
